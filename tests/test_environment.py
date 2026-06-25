b1ded2312d855d916474ab30b1f841c1d490c278
Aleksey Polezhaev <iqflashrussia@gmail.com>
2026-06-25 17:02:08 +0300

    Move GUI previews out of pytest discovery

---
+++ tests/test_environment.py
@@ -0,0 +1,5 @@
+def test_runtime_dependencies_import() -> None:
+    import imagequant  # noqa: F401
+    import PIL  # noqa: F401
+    import PySide6  # noqa: F401
+    import tablerqicon  # noqa: F401
