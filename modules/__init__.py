70ef20824e82853eb8a8d891da8bd57d7206b25a
Aleksey Polezhaev <iqflashrussia@gmail.com>
2026-06-26 06:43:16 +0300

    Declare modules package exports

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,17 @@
-from . import (
-    archive_handler,
-    config,
-    fla_operations,
-    folder_scanner,
-    image_optimizer,
-    rename_utils,
-    settings_finder,
-)
+from . import archive_handler as archive_handler
+from . import config as config
+from . import fla_operations as fla_operations
+from . import folder_scanner as folder_scanner
+from . import image_optimizer as image_optimizer
+from . import rename_utils as rename_utils
+from . import settings_finder as settings_finder
+
+__all__ = [
+    "archive_handler",
+    "config",
+    "fla_operations",
+    "folder_scanner",
+    "image_optimizer",
+    "rename_utils",
+    "settings_finder",
+]
